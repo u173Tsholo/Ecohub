5351fd680f02630c52fb03c938105e0423449891
tsholofelo.gomba <tsholofelo.gomba@dariel.co.za>
2022-08-06 19:12:50 +0200

    supp controller

--- EcoHub/Controllers/SupplierProductController.cs
+++ EcoHub/Controllers/SupplierProductController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcoHub.Models;
+using EcoHub.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcoHub.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class SupplierProductController : Controller
     {
-        public IActionResult Index()
+        private readonly ISupplierProductService _supplierProduct;
+        public SupplierProductController(ISupplierProductService supplierProductService)
         {
-            return View();
+            _supplierProduct = supplierProductService;
         }
+
+        [HttpGet(Name = "GetAllProductsBySupplier")]
+        public async Task GetAllProductsBySupplier(SupplierProduct supplierProduct)
+        {
+            await _supplierProduct.GetAllProductsBySupplier(supplierProduct);
+        }
+
+        [HttpPost(Name = "AddNewProduct")]
+        public void AddNewProduct(SupplierProduct supplierProduct)
+        {
+            _supplierProduct.AddNewProduct(supplierProduct);
+        }
+
     }
 }
